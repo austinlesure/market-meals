917351ea6e37ccd795b9f988a3fb0d06c6578603
Austin LeSure <austin.lesure@gmail.com>
2018-02-01 18:50:54 -0800

    commented out db references

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://market-meals:market-meals@localhost:8889/market-meals')
-app.config['SQLALCHEMY_ECHO'] = True
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://market-meals:market-meals@localhost:8889/market-meals')
+#app.config['SQLALCHEMY_ECHO'] = True
 
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
 app.secret_key = 'MarKetMeaLSiSAMAzinG'
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import request, redirect, render_template, session, flash
-from app import app, db
-from models import User
+from app import app #db
+#from models import User
 from hashutils import check_pw_hash
 
 @app.route('/')
