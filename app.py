fd7d83361d79b5cfd11554e8cc14192d793208c1
HAUKAP <lynnettehaukap@gmail.com>
2018-02-18 08:54:41 -0800

    initial commit for map test route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+import names
+from marketAddress import MarketAddress
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,5 +10,24 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+py
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
-
 app.secret_key = 'MarKetMeaLSiSAMAzinG'
+
+@app.route('/', methods=['GET', 'POST'])
+def func():
+  if request.method == 'POST':
+    zipcode = request.form['zipcode']
+    return redirect(url_for('success', name = zipcode) )
+  else:
+    zipcode = request.args.get('zipcode')
+    return redirect(url_for('success', name = zipcode) )
+    
+@app.route("/google-maps", methods=['GET'])
+def func(marketName):
+def names():
+  result = query_db("SELECT mname FROM market.marketName")
+  data = json.dumps(result)
+  resp = Response(data, status=200, mimetype='application/json')
+  return resp
+   
+
+
