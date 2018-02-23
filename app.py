38b9e0823d690fede6d78e0c6a962b0897e908cd
Xoadra <xijp77@gmail.com>
2018-02-23 02:40:46 -0800

    Cleaned up code formatting.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+
+
+
+
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://market-meals:market-meals@localhost:8889/market-meals')
-app.config['SQLALCHEMY_ECHO'] = True
 
-db = SQLAlchemy(app)
+app = Flask( __name__ )
+db = SQLAlchemy( app )
+
+app.config[ 'DEBUG' ] = True
+app.config[ 'SQLALCHEMY_DATABASE_URI' ] = os.environ.get(
+	'DATABASE_URL',
+	'mysql+pymysql://market-meals:market-meals@localhost:8889/market-meals'
+)
+app.config[ 'SQLALCHEMY_ECHO' ] = True
+
 app.secret_key = 'MarKetMeaLSiSAMAzinG'
+
+
--- main.py
+++ main.py
@@ -1,47 +1,55 @@
-from flask import render_template, session, flash, request, redirect
+
+
+
+
 from app import app, db
+from flask import render_template, session, flash, request, redirect
 from models import Farmer, Product, Prodcat, Market, Marketday, Recipe, Recipeproduct
 from hashutils import check_pw_hash
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route( '/' )
+def index( ):
+    return render_template( 'index.html' )
 
-@app.route('/logout')
-def logout():
-    return render_template('index.html')
+@app.route( '/about' )
+def about( ):
+    return render_template( 'about.html' )
 
-@app.route('/customer')
-def customer():
-    return render_template('customer.html')
+@app.route( '/login' )
+def login( ):
+    return render_template( 'login.html' )
 
-@app.route('/farmer-user')
-def farm_user():
-    return render_template('farmer_user.html')
+@app.route( '/logout' )
+def logout( ):
+    return render_template( 'index.html' )
 
-@app.route('/farm')
-def farmer():
-    return render_template('farm.html')
+@app.route( '/customer' )
+def customer( ):
+    return render_template( 'customer.html' )
 
-@app.route('/market')
-def market():
-    return render_template('market.html')
+@app.route( '/farmer-user' )
+def farm_user( ):
+    return render_template( 'farmer_user.html' )
 
-@app.route('/map')
-def map():
-    return render_template('map.html')
+@app.route( '/farm' )
+def farmer( ):
+    return render_template( 'farm.html' )
 
-@app.route('/recipe')
-def recipe():
-    return render_template('recipe.html')
+@app.route( '/market' )
+def market( ):
+    return render_template( 'market.html' )
+
+@app.route( '/map' )
+def map( ):
+    return render_template( 'map.html' )
+
+@app.route( '/recipe' )
+def recipe( ):
+    return render_template( 'recipe.html' )
 
 if __name__ == '__main__':
-    app.run()
+    app.run( )
+
+
