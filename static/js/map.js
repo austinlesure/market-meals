c139a8c073c56db5598720113b1d560e221a080a
Xoadra <xijp77@gmail.com>
2018-03-01 01:00:20 -0800

    Coordinates on map determined dynamically via zip code.

--- static/js/map.js
+++ static/js/map.js
@@ -2,10 +2,6 @@
 
 
 
-// Prototyping Google Maps api use - needs behavior to handle no geocode results
-
-
-
 window.addEventListener( 'load', getZipCode )
 
 
@@ -14,7 +10,6 @@ function getZipCode( ) {
 	http.onreadystatechange = function( ) {
 		if ( http.readyState === XMLHttpRequest.DONE && http.status === 200 ) {
 			// Search for geocode data that matches zip code
-			console.log( 'ZipCode HTTP: ' + http.responseText )
 			viewGeocode( http.responseText )
 		}
 	}
@@ -23,33 +18,24 @@ function getZipCode( ) {
 }
 
 function viewGeocode( zipcode ) {
-	console.log( 'ZipCode GEO: ' + zipcode )
-	// Build the geocoder object for geocode lookups
+	// Geocode location data based upon zip code input
 	var geo = new google.maps.Geocoder( )
-	// Record geolocation data using the given zip code
-	var code = geo.geocode( {
+	geo.geocode( {
 			address: zipcode,
 			componentRestrictions: { postalCode: zipcode },
 			region: '021'
 		},
-		function( data, fate ) {
+		function( geodata, fate ) {
 			if ( fate == 'OK' ) {
-				data.forEach( element => {
-					// Inspect the single geocode element in full
-					console.log( element )
-					// Transform location into json for portability
-					var coord = data[ 0 ].geometry.location.toJSON( )
-					console.log( 'JSON Location:', coord )
-					// View location coordinates in greater detail
-					console.log( 'LatLng Location: ' + element.geometry.location )
-					console.log( '    Latitude: ' + element.geometry.location.lat( ) )
-					console.log( '    Longitude: ' + element.geometry.location.lng( ) )
-				} )
+				// Transform location into json for portability
+				var coords = data[ 0 ].geometry.location.toJSON( )
+				// Output geocode objects found via zip code
+				console.log( 'Geocode:', geodata[ 0 ] )
 				// Utilize geocode results to render map contents
-				drawMap( data, zipcode )
+				drawMap( coords, zipcode )
 			}
-			// Watch for and redirect on errors from geocoding
 			else {
+				// Watch for and redirect on geocoding errors
 				console.error( 'There was a problem!  ' + fate )
 				window.location.pathname = ''
 			}
@@ -60,15 +46,15 @@ function viewGeocode( zipcode ) {
 function drawMap( geocode, zipcode ) {
 	// Avoid annoying, meaningless console errors
 	if ( geocode ) {
-		console.log( 'ZipCode API: ' + zipcode )
-		// Non-functional fetching of geocode objects
-		console.log( 'Geocode is... ', geocode[ 0 ] )
+		// View location details used for api mapping
+		console.log( 'ZipCode: ' + zipcode )
+		console.log( 'Location:', geocode )
 		// Example coordinates for the Seattle area
 		var seattle = new google.maps.LatLng( 47.6029432, -122.332146 )
-		// Generate new interactive map from the api
+		// Instantiate new interactive map from api
 		var map = new google.maps.Map(
 			document.getElementById( 'map' ),
-			{ center: seattle, zoom: 11 }
+			{ center: geocode, zoom: 12 }
 		)
 	}
 }
