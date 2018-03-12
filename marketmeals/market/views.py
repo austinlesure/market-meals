d9e751f6f21c6167c31024b8608e0b72e6ee66d2
Xoadra <xijp77@gmail.com>
2018-03-12 01:50:24 -0700

    Merged code now operational in branch.

--- marketmeals/market/views.py
+++ marketmeals/market/views.py
@@ -7,6 +7,9 @@ import json
 from marketmeals import db
 from marketmeals.market.utils import html, urlify, catalog
 from marketmeals.market.models import Market
+from marketmeals.farmer.models import Farmer, farmer_days
+from marketmeals.product.models import FarmerProduct, Category
+from marketmeals.recipe.models import Recipe, Ingredient
 from flask import Blueprint, request, render_template, redirect, url_for, session
 
 
@@ -43,49 +46,50 @@ def view( market ):
 		return redirect( '/' )
 
 
-## View method commented out originally from templates branch
-""" @market.route( '/join', methods = [ 'GET' ] )
+## View method not yet linked to originally from templates branch
+@market.route( '/join', methods = [ 'GET' ] )
 def join( ):
 	farmers = [ ]
 	recipes = [ ]
-	identifiers = {  } """
+	identifiers = {  }
 	## Grabbing market via get request from the url most likely
-	""" market = request.args.get( 'market' )
+	market = request.args.get( 'market' )
 	if market:
-		market = Market.query.filter_by( name = market ).first( ) """
+		market = Market.query.filter_by( name = market ).first( )
 		## Linking farmers with markets, so use farmer_days table
-		""" days = FarmerMarketLink.query.filter_by( market_id = market.market_id ).all( )
+		days = farmer_days.query.filter_by( market_id = market.market_id ).all( )
+		""" days = FarmerMarketLink.query.filter_by( market_id = market.market_id ).all( ) """
 		for day in days:
 			farmer = Farmer.query.filter_by( farmer_id = day.farmer_id ).first( )
 			pid = farmer.farmer_id
-			farmers.append( farmer ) """
+			farmers.append( farmer )
 			## Obtain the farmer's goods to be for sale at the market
 			## Replace by querying the farmer_products table instead
-			""" merch = FarmerProductLink.query.filter_by( farmer_id = farmer.farmer_id ).all( )
+			merch = FarmerProduct.query.filter_by( farmer_id = farmer.farmer_id ).all( )
 			for good in merch:
-				product = Product.query.filter_by( product_id = good.product_id ).first( ) """
+				product = Product.query.filter_by( product_id = good.product_id ).first( )
 				## Query the more semantic ingredients table for querying
-				""" for ingredient in get_recipe_products( ):
+				for ingredient in Ingredient.query.all( ):
 					if ingredient.product_id == product.product_id:
 						if ingredient not in recipes:
-							recipes.append( Recipe.query.filter_by( recipe_id = ingredient.recipe_id ).first( ) ) """
+							recipes.append( Recipe.query.filter_by( recipe_id = ingredient.recipe_id ).first( ) )
 				## Seems to be collecting farmer ids for a certain purpose
-				""" if pid in identifiers:
+				if pid in identifiers:
 					identifiers[ pid ].append( product )
-				else: """
+				else:
 					## Pretty sure this creates a 2D list for no good reason
-					""" identifiers[ pid ] = [ product ] """
+					identifiers[ pid ] = [ product ]
 		## Badly needs trimming down via a context dictionary variable
-		""" return render_template(
-			'market/join.html',
+		return render_template(
+			'market/market.html',
 			market = market,
 			farmers = farmers,
-			categories = get_prodcats( ),
-			dic = dic,
+			categories = Category.query.all( ),
+			identifiers = identifiers,
 			recipes = recipes
 		)
 	else:
-		return redirect( '/' ) """
+		return redirect( '/' )
 
 
 
--- marketmeals/recipe/views.py
+++ marketmeals/recipe/views.py
@@ -18,16 +18,16 @@ def cuisine( ):
 
 ## Temporarily commented out changes from templates branch
 """ @recipe.route( '/kitchen' )
-def kitchen( ): """
-	## Fetching the recipe name from the url through a get request
-	""" recipe = request.args.get( 'recipe' )
+def kitchen( ):
+	Fetching the recipe name from the url through a get request
+	recipe = request.args.get( 'recipe' )
 	if recipe:
-		recipe = Recipe.query.filter_by( name = recipe ).first( ) """
-		## Unsure if this is meant to be a collective value or not
-		""" return render_template( 'recipe/kitchen.html', recipes = recipe )
-	else: """
-		## Querying for all recipes for displaying on templates
-		""" return render_template( 'recipe/kitchen.html', recipes = get_recipes( ) ) """
+		recipe = Recipe.query.filter_by( name = recipe ).first( )
+		Unsure if this is meant to be a collective value or not
+		return render_template( 'recipe/kitchen.html', recipes = recipe )
+	else:
+		Querying for all recipes for displaying on templates
+		return render_template( 'recipe/kitchen.html', recipes = get_recipes( ) ) """
 
 
 
